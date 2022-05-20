f9af5f3a2e3b0a9a831d199181f91c1cbba34c10
Kurt Roekle <kroekle@gmail.com>
2022-05-20 11:37:45 -0500

    adding schema and endpoint to get schema

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import { StarWarsSchema } from "./starwars/starWarsSchema";
-import express from 'express';
+import express, {Request, Response} from 'express';
 import {graphqlHTTP} from 'express-graphql';
+import { buildSchema, printSchema } from 'graphql'
 
 const app = express();
 app.use('/graphql', graphqlHTTP({
 schema: StarWarsSchema,
 }));
+app.use('/schema', (req: Request, res: Response) => {
+    res.send(printSchema(StarWarsSchema));
+  });
 app.listen(4000);
 
 console.log('Server ready at http://localhost:4000/graphql');
